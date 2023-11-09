8e09497c82f4785359bbca82f586edc994a43c0e
AlceaXich <138715588+AlceaXich@users.noreply.github.com>
2023-11-09 15:56:25 +0000

    Cartera de javascript con Github Copilot

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "AlceaXich",
+  title: "Web Developer & Software Developer",
+  email: "shirmoc.t05@gmail.com",
+  gitHub: "AlceaXich",
+  instagram: "AlceaXich",
+  linkedIn: "Shi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "AlceaXich",
+  youTube: "AlceaXich",
 };
 
 const primaryColor = "#4E567E";
